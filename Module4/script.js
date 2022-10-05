40e51aabcc7a1bf40cf8f38be663c9ead229d529
Aman-002 <you@example.com>
2022-10-05 22:22:12 +0530

    Module4

---
+++ Module4/script.js
@@ -0,0 +1,18 @@
+(function(){
+              var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+              for (var i=0 ; i < names.length ; i++) {
+                var firstLetter = names[i].charAt(0);
+                firstLetter= firstLetter.toLowerCase();
+                if (firstLetter == "j"){
+                      byeSpeaker.name=names[i];
+                      byeSpeaker.speak();
+
+                } 
+                else {
+                      helloSpeaker.name=names[i];
+                      helloSpeaker.speak();  
+                }
+              }
+
+
+})();
